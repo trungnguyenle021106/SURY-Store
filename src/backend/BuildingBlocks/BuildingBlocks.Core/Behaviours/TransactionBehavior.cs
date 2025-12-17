eb367cb7a5927d5f51c86018208d417f93656be4
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-12-17 10:30:13 +0700

    feat(building-blocks): Change the way how Transaction Behavior work

--- src/backend/BuildingBlocks/BuildingBlocks.Core/Behaviours/TransactionBehavior.cs
+++ src/backend/BuildingBlocks/BuildingBlocks.Core/Behaviours/TransactionBehavior.cs
@@ -5,20 +5,35 @@ using MediatR;
 namespace Catalog.Application.CQRS.Behaviours
 {
     public class TransactionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : ICommand<TResponse> 
+    where TRequest : ICommand<TResponse>
     {
-        private readonly IApplicationDbContext _dbContext; 
+        private readonly IApplicationDbContext _dbContext;
 
-        public TransactionBehavior(IApplicationDbContext _dbContext)
+        public TransactionBehavior(IApplicationDbContext dbContext)
         {
-            this._dbContext = _dbContext;
+            _dbContext = dbContext;
         }
 
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
-            var response = await next();
-            await _dbContext.SaveChangesAsync(cancellationToken);
-            return response;
+            using var transaction = await _dbContext.BeginTransactionAsync();
+
+            try
+            {
+                var response = await next();
+
+                await _dbContext.SaveChangesAsync(cancellationToken);
+
+
+                await transaction.CommitAsync(cancellationToken);
+
+                return response;
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync(cancellationToken);
+                throw;
+            }
         }
     }
 }
--- src/backend/BuildingBlocks/BuildingBlocks.Core/Infrastructure/Data/IApplicationDbContext.cs
+++ src/backend/BuildingBlocks/BuildingBlocks.Core/Infrastructure/Data/IApplicationDbContext.cs
@@ -1,7 +1,10 @@
-﻿namespace BuildingBlocks.Core.Infrastructure.Data
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BuildingBlocks.Core.Infrastructure.Data
 {
     public interface IApplicationDbContext
     {
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+        Task<IDbContextTransaction> BeginTransactionAsync();
     }
 }
