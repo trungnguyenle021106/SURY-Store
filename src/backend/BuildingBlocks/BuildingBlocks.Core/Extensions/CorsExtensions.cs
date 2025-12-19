c5515d324c7854b424889e991cadcac5858204f9
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-12-19 11:10:46 +0700

    feat(building-blocks): CorsExtensions

---
+++ src/backend/BuildingBlocks/BuildingBlocks.Core/Extensions/CorsExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Core.Extensions
+{
+    public static class CorsExtensions
+    {
+        public const string AllowAllPolicy = "AllowAll";
+
+        public static IServiceCollection AddCustomCors(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(AllowAllPolicy, policy =>
+                {
+                    var allowedOrigins = configuration.GetSection("AllowedOrigins").Get<string[]>();
+
+                    if (allowedOrigins is not null && allowedOrigins.Length > 0)
+                    {
+                        policy.WithOrigins(allowedOrigins)
+                              .AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowCredentials(); 
+                    }
+                    else
+                    {
+                        policy.SetIsOriginAllowed(origin => true)
+                              .AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowCredentials();
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
