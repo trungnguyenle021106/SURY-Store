7674ff5e469fe4c2c0e72be255143f2dea07095b
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-12-19 11:08:37 +0700

    chore(identity): Use AddCustomJwtAuthentication

--- src/backend/Services/Identity/Identity.API/Program.cs
+++ src/backend/Services/Identity/Identity.API/Program.cs
@@ -38,42 +38,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 .AddEntityFrameworkStores<IdentityDbContext>()
 .AddDefaultTokenProviders();
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-
-        ValidateAudience = true,
-        ValidAudience = builder.Configuration["JwtSettings:Audience"],
-
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Secret"]!)),
-
-        ValidateLifetime = true,
-        ClockSkew = TimeSpan.Zero
-    };
-
-
-    options.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
-            if (context.Request.Cookies.ContainsKey("access_token"))
-            {
-                context.Token = context.Request.Cookies["access_token"];
-            }
-            return Task.CompletedTask;
-        }
-    };
-});
+builder.Services.AddCustomJwtAuthentication(builder.Configuration);
 
 builder.Services.AddCors(options =>
 {
