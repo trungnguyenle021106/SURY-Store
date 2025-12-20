0ff90e934b9f777a28672b9237f2ffb4e4c8b10d
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-12-20 07:35:32 +0700

    feat(building-blocks): GetDescription has cache avoid Reflection

--- src/backend/BuildingBlocks/BuildingBlocks.Core/Extensions/EnumExtensions.cs
+++ src/backend/BuildingBlocks/BuildingBlocks.Core/Extensions/EnumExtensions.cs
@@ -1,14 +1,28 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Concurrent;
+using System.ComponentModel;
 
 namespace BuildingBlocks.Core.Extensions
 {
     public static class EnumExtensions
     {
+        private static readonly ConcurrentDictionary<Enum, string> _cache = new();
+
         public static string GetDescription(this Enum value)
         {
-            var field = value.GetType().GetField(value.ToString());
-            var attribute = (DescriptionAttribute?)Attribute.GetCustomAttribute(field!, typeof(DescriptionAttribute));
-            return attribute == null ? value.ToString() : attribute.Description;
+            if (value == null) return string.Empty;
+
+
+            return _cache.GetOrAdd(value, (enumValue) =>
+            {
+                var field = enumValue.GetType().GetField(enumValue.ToString());
+
+                if (field == null) return enumValue.ToString();
+
+                var attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute))
+                                as DescriptionAttribute;
+
+                return attribute != null ? attribute.Description : enumValue.ToString();
+            });
         }
     }
 }
