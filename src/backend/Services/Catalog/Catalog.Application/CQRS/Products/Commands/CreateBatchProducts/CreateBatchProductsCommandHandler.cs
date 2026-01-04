e170e56be476833cac8451d69af478cda0e067c3
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2026-01-04 10:28:56 +0700

    feat(catalog): CreateBatchProduct API, Change default quantity to 1

---
+++ src/backend/Services/Catalog/Catalog.Application/CQRS/Products/Commands/CreateBatchProducts/CreateBatchProductsCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.Application.Common.Interfaces;
+using Catalog.Domain.Entities;
+using Catalog.Application.CQRS.Products.Commands.CreateProduct;
+using Mapster;
+using MediatR;
+
+namespace Catalog.Application.CQRS.Products.Commands.CreateBatchProducts
+{
+    public class CreateBatchProductsHandler : IRequestHandler<CreateBatchProductsCommand, CreateBatchProductsResult>
+    {
+        private readonly ICatalogDbContext _dbContext;
+
+        public CreateBatchProductsHandler(ICatalogDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<CreateBatchProductsResult> Handle(CreateBatchProductsCommand request, CancellationToken cancellationToken)
+        {
+            var products = request.Products.Adapt<List<Product>>();
+
+            await _dbContext.Products.AddRangeAsync(products, cancellationToken);
+
+            var resultDtos = products.Adapt<List<CreateProductResult>>();
+
+            return new CreateBatchProductsResult(resultDtos);
+        }
+    }
+}
