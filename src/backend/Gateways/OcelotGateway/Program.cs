cd77a3d10b8aac87e33b9b4a30129b81675303b6
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2026-01-11 20:37:46 +0700

    Chore

--- src/backend/Gateways/OcelotGateway/Program.cs
+++ src/backend/Gateways/OcelotGateway/Program.cs
@@ -4,8 +4,7 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-
-
+// Khi nào nhiều domain thì loại bỏ CORS này
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", b =>
