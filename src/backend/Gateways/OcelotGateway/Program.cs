2092ed1dddc1e54990e0c693c8f76fab14130079
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2026-01-13 08:36:11 +0700

    feat: authentication for ocelot

--- src/backend/Gateways/OcelotGateway/Program.cs
+++ src/backend/Gateways/OcelotGateway/Program.cs
@@ -10,12 +10,13 @@ builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange
 builder.Services.AddCustomCors(builder.Configuration);
 
 builder.Services.AddOcelot();
-
+builder.Services.AddCustomJwtAuthentication(builder.Configuration);
 var app = builder.Build();
 
 
 app.UseCors(CorsExtensions.AllowAllPolicy);
-
+app.UseAuthentication();
+app.UseAuthorization();
 await app.UseOcelot();
 
 app.Run();
