ac86397baa92ebbfde459ee86233db62205bdc3d
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2026-01-16 09:28:54 +0700

    chore

--- src/frontend/src/app/environments/environment.ts
+++ src/frontend/src/app/environments/environment.ts
@@ -4,7 +4,7 @@ const ENV_TYPE = 'CLOUD'; // Thay đổi thành: 'LOCAL', 'DOCKER', hoặc 'CLOU
 const configs = {
   LOCAL: 'http://localhost:5000/api',
   DOCKER: 'http://host.docker.internal:5000/api', // Dùng host.docker.internal để gọi từ container ra máy host
-  CLOUD: 'URL WEB HOST'
+  CLOUD: 'https://sury.store/api'
 };
 
 export const environment = {
